dd32d96b7e3f6cd962e068564e795014a7147eaa
doaa-yehia <doaa_Yehia28@yahoo.com>
2025-07-08 18:27:04 +0300

    Assignment Q05

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -100,6 +100,44 @@
 
             #endregion
 
+            #region Q05
+            ///- Write a program to enter marks of five subjects and calculate total, average and percentage.
+
+            //double sum = 0;
+
+            //bool flag = true;
+            //for (int i = 1; i <= 5; i++) //95 76 58 90 89
+            //{
+            //    Console.Write($"Please Enter The  marke of subject {i} : ");
+            //    bool isPared = double.TryParse(Console.ReadLine(), out double sub);
+
+            //    if (isPared)
+            //    {
+            //        sum += sub;
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("You Must Enter Number");
+            //        flag = false;
+            //        break;
+
+            //    }
+
+            //}
+            //if (flag)
+            //{
+            //    double avg = sum / 5;
+            //    Console.WriteLine($"The Avarage = {avg}");
+
+            //    double perc = (sum / 500) * 100;
+            //    Console.WriteLine($"The Percentage = {perc}");
+
+            //}
+
+
+            #endregion
+
+
 
 
         }
