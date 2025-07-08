338de95082ff585041d0f0067f02ba9b5a329d1d
doaa-yehia <doaa_Yehia28@yahoo.com>
2025-07-08 18:23:49 +0300

    Assignment Q01

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            #region Q01 
+            ///Write a program that allows the user to insert an integer then print all numbers 
+            ///between 1 to that number.
+
+            //Console.Write("Please Enter Number : ");
+            //bool isParsed = int.TryParse(Console.ReadLine(), out int number);
+
+            //if (isParsed)
+            //{
+            //    for(int i = 0; i <= number; i++)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you didn't Enter Number");
+            //}
+
+            #endregion
+
+
+
         }
     }
 }
