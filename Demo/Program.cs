efd5e1a1775bced06f628eefa565305f9cc66720
doaa-yehia <doaa_Yehia28@yahoo.com>
2025-07-08 18:12:21 +0300

    video 02 Evalution of Switch C# 8.0

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -74,6 +74,74 @@
 
             #endregion
 
+            #region C# 8.0 
+
+            #region Example 01 pattern Matching Without alias name
+
+            //object num = 5.3;
+            //switch (num)
+            //{
+            //    case int :  //UnBoxing
+            //        Console.WriteLine("Integer");
+            //        break;
+            //    case decimal :
+            //        Console.WriteLine("decimal");
+            //        break;
+            //    case double when (double) num > 3.5 && (double) num < 15.5:
+            //        Console.WriteLine("double");
+            //        break;
+
+            //}
+
+            #endregion
+
+            #region Example 02 Switch Expressions [ Constant Pattern - Discard Pattern ]
+
+            //Console.Write("Enter Your number : ");
+
+            //int.TryParse(Console.ReadLine(), out int option);
+
+            //string message = option switch
+            //{
+            //    1 => "Option 01",
+            //    2 => "Optrion 02",
+            //    3 => "Optrion 02",
+            //    _ => "Invalid Option"
+
+            //};
+            //Console.WriteLine(message);
+
+            #endregion
+
+            #region Example 03 [property Pattern]
+
+            //Person person = new Person { Id = 2, Name = "Mostafa", Age = 30 };
+            //string message = person switch
+            //{
+            //    { Name: "Mohamed", Age: 10 } => "Hello Mohamed",
+            //    { Name: "Mostafa" } => "Hello Mostafa",
+            //    _ =>"Sorry We Don't Know you"
+            //};
+            //Console.WriteLine(message);
+
+            #endregion
+
+            #region Example 04 [ Nullable type - Relational Pattern (Partially) ]
+
+            //int? num = 10;
+            //string massage = num switch
+            //{
+            //    null => "Nullable type",
+            //    int X when X > 0 => "Positive number",
+            //    int X when X < 0 => "Negative number",
+            //    0 => "Zero number",
+
+            //};
+            //Console.WriteLine(massage);
+            #endregion
+
+            #endregion
+
 
 
             #endregion
